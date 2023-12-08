a7f04f605ed3b718dfeca1ba81b9d6dbd3711ac4
Beatriz Simões <121191936+beatrizlsimoes@users.noreply.github.com>
2023-12-08 15:55:49 +0000

    Homework Week 1

---
+++ Homework1.py
@@ -0,0 +1,17 @@
+# Exercise 1
+chairs = 15 
+nails = 4
+total_nails = chairs * nails
+message = print ('You need to buy {} nails'.format(total_nails))
+
+# Exercise 2
+my_name = 'Penelope'
+my_age = 29
+message = print ('My name is {} and I am {} years old'.format(my_name, my_age))
+
+# Exercise 3
+eggsperbox = 6
+eggsperomelette = 4
+boxes = 6
+omelettes = boxes * eggsperbox // eggsperomelette
+message = print ('You can make {} omelettes with {} boxes of eggs'.format (omelettes, boxes))
